6faace207c42adf39f9d48e366edc8d0d859d342
Arel Tiyan <54056041+areltiyan@users.noreply.github.com>
2020-05-25 13:18:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+const { IgApiClient } = require('instagram-private-api')
+const ig = new IgApiClient();
+const fs = require('fs');
+const readlineSync = require('readline-sync');
+const delay = require('delay');
+const moment = require('moment');
+
+function GetRandomID(dataid)
+{
+    const values = Object.values(dataid)
+        const randomValue = values[parseInt(Math.random() * values.length)]
+
+    return(randomValue);
+};
+
+async function Execute(target, ig, komen, latest_id)
+{
+    fs.readFile('./target/' + target + '.txt', {encoding: 'utf-8'}, async function(err,data){
+        if (!err) {
+            if(latest_id != data)
+            {
+                try{
+                  const start = await ig.media.comment({mediaId: latest_id, text: komen})
+                  console.log(`[ ${moment().format('HH:mm:ss')} ] [SUKSES KOMEN @${target}] [${komen}]`)
+                  fs.writeFileSync('./target/' + target + '.txt', latest_id);
+                }catch(err)
+                {
+                  console.log(`[ ${moment().format('HH:mm:ss')} ] [FAILED KOMEN @${target}] [${err.toString()}]`)
+                }
+            }else{
+                console.log(`[ ${moment().format('HH:mm:ss')} ] TIDAK MENEMUKAN POST BARU`, target)
+            }
+            
+        } else {
+            console.log(err)
+        }
+    }); 
+}
+
+(async () => {
+
+  
+  try{
+    let enabled = 0 ;
+    console.log(`FRIST COMMENT CREATED BY AREL TIYAN\nSGB TEAM 2020\nCEK komen.txt DENGAN PEMISAH |\n`)
+    const user_name   = readlineSync.question("Username : ")
+    const pass_word   = readlineSync.question("Password : ")
+    var tar_get       = readlineSync.question("Target (Pakai , Jika lebih dari 1) : ").split(',')
+    var jeda          = readlineSync.question("Jeda Berapa Detik : ")
+    ig.state.generateDevice(user_name);
+    await ig.simulate.preLoginFlow();
+
+    const loggedInUser = await ig.account.login(user_name, pass_word);
+
+    var read_komen = fs.readFileSync("./komen.txt","utf-8").split("|");
+    
+    for(let i = 0;i < tar_get.length;i++)
+    {
+      const id = await ig.user.getIdByUsername(tar_get[i]);
+      const userFeed = ig.feed.user(id);
+      const myPostsFirstPage = await userFeed.items();
+      fs.writeFileSync('./target/' + tar_get[i] + '.txt', myPostsFirstPage[0].id);
+      console.log('Checking latest media_id', tar_get[i])
+      if(i == tar_get.length - 1) enabled = 1
+    }
+
+    if(enabled == 1)
+    {
+      while(true){
+        tar_get.map(async function(param, index)
+        {
+            try{
+              var komen = GetRandomID(read_komen)
+              const id = await ig.user.getIdByUsername(param);
+              const userFeed = ig.feed.user(id);
+              const myPostsFirstPage = await userFeed.items();
+              const latest_id = myPostsFirstPage[0].id
+              await delay(1500)
+                const gas = await Promise.all([
+                    Execute(param, ig, komen, latest_id)
+                ])
+            }catch(err)
+            {
+                console.log(err.toString())
+            }
+            
+        })
+        
+        await delay(`${jeda}000`)
+      }
+    }
+
+    
+
+  }catch(err){
+    console.log(err.toString())
+  }
+
+})();
